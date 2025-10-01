5f17320615f7b5172ebe86eea47c8c4de86c6e53
Sarah Comble <167861485+Sarah-Comble@users.noreply.github.com>
2025-10-01 22:12:16 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = input("There is a crossroad in front of you. You can go left or right. Which way do you choose? ")
+direction = direction.lower()
+
+if direction == "left":
+  action = input("You have reached a lake. There is an island in the middle. Do you want to swim or wait for a boat ? type swim or wait ").lower()
+  if action == "wait":
+    door = input("You have arrived at the island. There is a house with 3 doors. Which one do you choose ? red, yellow or white? ").lower()
+    if door == "red":
+      print("You have been burned by fire, sorry game over.")
+    elif door == "yellow":
+      print("You have been attacked by bees, sorry game over.")
+    elif door == "white" :
+      print ("Congratulation, you have found the treasure.")
+    else:
+      print("Sorry we don't understand, game over.")
+  else:
+    print("An aligator eats you, Game Over.")  
+else:
+  print("Game over. You have been attacked by indigenes.")
